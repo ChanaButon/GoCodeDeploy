c6dccb18bad0de481d953638e54274ea48112f57
ChanaButon <booton@g.jct.ac.il>
2023-03-05 03:21:45 +0200

    addclient

---
+++ targil0.asm
@@ -0,0 +1,30 @@
+InputA (1)
+$$$BUY bread$$$
+12
+$$$BUY cheese$$$
+10
+###CELL cheese###
+75
+$$$BUY milk$$$
+24
+###CELL chocolate###
+70
+$$$BUY milk$$$
+21
+###CELL chocolate###
+10
+InputB (1)
+$$$BUY chocolate$$$
+10
+###CELL eggs###
+600
+$$$BUY cheese$$$
+10
+###CELL sugar###
+66
+###CELL flour###
+21
+$$$BUY milk$$$
+9
+TOTAL BUY: 96
+TOTAL CELL: 842
