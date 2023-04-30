efe959d522b41b114183d5e7f8cfe1440158f42c
ChanaButon <booton@g.jct.ac.il>
2023-05-01 02:44:12 +0300

    fin

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import Admin from './components/Admin';
 import LoginForm from './components/LoginForm';
 import AboutPage from './components/About';
 import Contact from './components/Contact';
-import CartList from './components/CartList';
+import CartList from './components/CartList.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
